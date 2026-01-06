3c0ac2b9dce0e2d46454b7e3ddc3c179642358be
Lukas Jonathan Haeuser <lukas.haeuser@empa.ch>
2026-01-06 17:14:40 +0100

    feature(cache): Regenerate cache at each invocation of cache_data script

--- cache_data.py
+++ cache_data.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from immunotherapypredictionrnaseq.tokenizer import TokenConfig
 from immunotherapypredictionrnaseq.data import TCGAData
-
+from os import remove
 
 if __name__ == "__main__":
+    cache = Path.cwd().joinpath("cache/tcga_data.npy")
     config_path = Path.cwd().joinpath("token_config")
     assert config_path.exists() and config_path.is_dir()
     token_config = TokenConfig(config_path)
     token_config.load_config()
+    if cache.exists():
+        remove(cache)
     tcga_data = TCGAData(Path.cwd().joinpath("lair"), token_config)
-    tcga_data.load(n=0, cache=Path.cwd().joinpath("cache/tcga_data.npy"))
+    tcga_data.load(n=0, cache=cache)
