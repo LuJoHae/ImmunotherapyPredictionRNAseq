fix: Fix data leakage

immunotherapypredictionrnaseq/data.py
@@ -1,5 +1,5 @@
 import random
-
+import copy
 import anndata as ad
 import numpy as np
 import torch
@@ -292,3 +292,17 @@ class TCGAData(Dataset):
         adata_combined = adata_combined[:, sorted(list(adata_combined.var_names))]
         assert adata_combined.shape == (937, 912)
         return adata_combined
+
+    def train_test_split(self, ratio=0.8, seed=0):
+
+        train_dataset = copy.deepcopy(self)
+        test_dataset = copy.deepcopy(self)
+
+        rng = np.random.default_rng(seed=seed)
+        indices = rng.permutation(len(self._full_data))
+        n_split = int(np.round(ratio * len(self._full_data)))
+
+        train_dataset._full_data = train_dataset._full_data[indices[:n_split]]
+        test_dataset._full_data = test_dataset._full_data[indices[n_split:]]
+
+        return train_dataset, test_dataset

tests/integration/test_model_run.py
@@ -37,7 +37,7 @@ def setup_model_and_data(device, transformer_dim, transformer_nhead, transformer
     tcga_data.to(device)
     model = model.to(device).to(torch.float32)
 
-    tcga_train, tcga_test = random_split(tcga_data, (0.8, 0.2), generator=torch.Generator().manual_seed(0))
+    tcga_train, tcga_test = tcga_data.train_test_split()
 
     return model, tcga_train, tcga_test
 